9bf9e6b9e2ad0be5a68835d75d3423f6bca3a4e3
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-11-30 14:42:08 +0800

    update pom.xml file info

---
+++ nio-lecture/src/main/java/com/laibao/nettyinaction/nio/ByteBufferReaderMain.java
@@ -0,0 +1,25 @@
+package com.laibao.nettyinaction.nio;
+import java.io.FileInputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * @author laibao wang
+ */
+public class ByteBufferReaderMain {
+
+    private static final String filePath = "your_file_path";
+
+    public static void main(String[] args) throws Exception {
+        try(FileInputStream fileInputStream = new FileInputStream(filePath);
+            FileChannel fileChannel = fileInputStream.getChannel();){
+                ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+                fileChannel.read(byteBuffer);
+                byteBuffer.flip();
+                while (byteBuffer.remaining() > 0) {
+                    byte b = byteBuffer.get();
+                    System.out.println("Character: " + (char)b);
+                }
+        }
+    }
+}
